301f7c76b843b19904c09e4e1652920370eace56
Denis <help@denis-getcourse.ru>
2022-10-22 12:02:46 +0300

    HW_CS_15

---
+++ HW_CS_16/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+int GetNumber() {
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void checkWeekend(int numArg) {
+    if(numArg == 6 || numArg ==7) {
+        Console.WriteLine("Выходной");
+    }
+    else {
+        Console.WriteLine("Не выходной");
+    }
+}
+
+int num = GetNumber();
+
+if(num > 0 && num < 8){
+    checkWeekend(num);
+}
+else {
+    Console.WriteLine("Неверное число недели");
+}
