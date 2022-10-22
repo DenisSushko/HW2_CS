42d843256fcf4738f68ae5ba1f606d8abf5e101c
Denis <help@denis-getcourse.ru>
2022-10-22 12:32:32 +0300

    HW_CS_13

--- HW_CS_13/Program.cs
+++ HW_CS_13/Program.cs
@@ -1,4 +1,9 @@
-﻿int GetNumber() {
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+int GetNumber() {
     Console.WriteLine("Enter your number: ");
     int num = int.Parse(Console.ReadLine()!);
     return num;
